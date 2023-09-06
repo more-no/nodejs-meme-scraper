cfd983453dacca77a1b8fdc9cb75317730f5dcef
Moreno Dore <moreno.dore@tuta.io>
2023-09-06 15:18:40 +0200

    Create memes folder

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import * as fs from 'fs';
-import * as path from 'path';
 
-const folderPath = './memes';
+const path = './memes';
 
-fs.mkdir(folderPath);
+fs.access(path, (error) => {
+  // To check if the given directory already exists or not
+  if (error) {
+    // If current directory does not exist then create it
+    fs.mkdir(path, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('New Directory created.');
+      }
+    });
+  } else {
+    console.log('Given Directory already exists.');
+  }
+});
